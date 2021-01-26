a9d42916e3d2f1e9f5ff280a7443f57254b99523
mavicena21 <mavicena21@gmail.com>
2021-01-26 13:10:54 +0700

    remove print, add message variable

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -15,7 +15,9 @@ def isPhoneNumber(text):
         if not text[i].isdecimal():
             return False
     return True
-print('415-555-4244 is a phone number:')
-print(isPhoneNumber('415-555-4242'))
-print('Moshi moshi is a phone number:')
-print(isPhoneNumber('Moshi moshi'))
+message = 'Call me at 415-555-1011 tommorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found' + chunk)
+print('Done')
