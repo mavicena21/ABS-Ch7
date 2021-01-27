625d180cffbc400e312f2bbda722ce3e2edf7650
mavicena21 <mavicena21@gmail.com>
2021-01-27 13:48:49 +0700

    add grouping parentheses

--- regex.py
+++ regex.py
@@ -2,3 +2,9 @@ import re
 nohp = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = nohp.search('My number is 415-555-4242')
 print('Phonenumber found: ' + mo.group())
+
+nohp2 = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo2 = nohp2.search('Nomor aing adalah 555-666-6969')
+print(mo2.group(1))
+print(mo2.group(2))
+print(mo2.group(0))
